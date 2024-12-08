203c0c40f3cd0f8d1d18ed4fbccb91dcb63b422f
Nidhi Bhagat <nidhibhagat108@gmail.com>
2024-12-09 00:15:23 +0530

    Added Responsivness to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Header.css';
+import '../App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faPaperPlane, faUser, faTarp, faDiagramProject, faXmark, faFileLines, faTableCellsLarge } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+  /*toggle menu */
+  const [Toggle, showMenu] =useState(false)
   return (
     <header className="header">
-      <div>Nidhi</div>
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Skills</a></li>
-          <li><a href="#projects">Services</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
+      <nav className='nav container'>
+        <a href='index.html' className='nav-logo'>Nidhi</a>
+        <div className={Toggle ? "nav-menu show-menu": "nav-menu"}>
+          <ul className='nav-list'>
+            <li className='nav-item'><a className='nav-link active-link' href="#home"><FontAwesomeIcon className='nav-icon' icon={faHouse} /> Home</a></li>
+            <li className='nav-item'><a className='nav-link' href="#about"><FontAwesomeIcon className='nav-icon' icon={faUser} /> About</a></li>
+            <li className='nav-item'><a className='nav-link' href="#projects"><FontAwesomeIcon className='nav-icon' icon={faFileLines} /> Skills</a></li>
+            <li className='nav-item'><a className='nav-link' href="#projects"><FontAwesomeIcon className='nav-icon' icon={faTarp} /> Services</a></li>
+            <li className='nav-item'><a className='nav-link' href="#projects"><FontAwesomeIcon className='nav-icon' icon={faDiagramProject} /> Projects</a></li>
+            <li className='nav-item'><a className='nav-link' href="#contact"><FontAwesomeIcon className='nav-icon' icon={faPaperPlane} /> Contact</a></li>
+          </ul>
+          <FontAwesomeIcon className='nav-close' icon={faXmark} onClick={()=> showMenu(!Toggle)}/>
+        </div>
+        <div className="nav-toggle" onClick={()=> showMenu(!Toggle)}>
+          <FontAwesomeIcon icon={faTableCellsLarge} />
+        </div>
       </nav>
     </header>
   );
